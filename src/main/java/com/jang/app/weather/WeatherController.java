610971850ae06e6b232e79fc55ef0b1f278d2b7a
JUMINAHN <jumin9774@naver.com>
2024-06-18 15:31:07 +0900

    weatherController commit
    by ZOOM _ weatherController

---
+++ src/main/java/com/jang/app/weather/WeatherController.java
@@ -0,0 +1,19 @@
+package com.jang.app.weather;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WeatherController {
+	WeatherService weatherService;
+
+	@RequestMapping("weather/list")
+	public String getList(HttpServletRequest request) {
+		System.out.println("list 가져오기");
+		weatherService.getlist(); // ??
+		return "weather/list";
+	}
+
+}
