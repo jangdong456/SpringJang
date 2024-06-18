04543b4d25bae56f72fc141d5c29540c166ed6b0
JUMINAHN <jumin9774@naver.com>
2024-06-18 16:13:39 +0900

    @Autowired add

--- src/main/java/com/jang/app/weather/WeatherController.java
+++ src/main/java/com/jang/app/weather/WeatherController.java
@@ -2,15 +2,18 @@ package com.jang.app.weather;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WeatherController {
+	@Autowired
 	WeatherService weatherService;
 
 	@RequestMapping("weather/list")
 	public String getList(HttpServletRequest request) {
+		request.getParameter("");
 		System.out.println("list 가져오기");
 		weatherService.getlist(); // ??
 		return "weather/list";
